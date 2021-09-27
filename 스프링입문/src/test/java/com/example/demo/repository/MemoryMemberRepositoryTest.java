726e5241c96f7f043ca8045bea77803e72e8f548
kakjzi <anmern3@naver.com>
2021-09-27 23:58:22 +0900

    .

---
+++ 스프링입문/src/test/java/com/example/demo/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LogRecordListener;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("jiwoo");
+
+        repository.save(member);
+
+        Member result =  repository.findById(member.getId()).get();
+        System.out.println("result = " + (result == member));
+
+        //assertThat(actual).isEqualTo(expected);
+        assertThat(member).isEqualTo(result);
+        // == Assertions.assertEquals(member, result);
+    }
+
+    //소소한 꿀팁 shift + F6 -> variable rename
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("jiwoo1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("jiwoo2");
+        repository.save(member2);
+
+        Member result = repository.findByName("jiwoo1").get();
+//        System.out.println(repository.findByName("jiwoo1").get());
+//        System.out.println(repository.findByName("jiwoo2").get());
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("jiwoo1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("jiwoo2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+}
