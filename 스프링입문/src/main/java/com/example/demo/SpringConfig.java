949f8d3de6abdf5417991155259ff34a5e199b37
kakjzi <anmern3@naver.com>
2021-10-09 20:15:37 +0900

    FIX: MemoryRepository to JDBCRepository

--- 스프링입문/src/main/java/com/example/demo/SpringConfig.java
+++ 스프링입문/src/main/java/com/example/demo/SpringConfig.java
@@ -1,16 +1,33 @@
 package com.example.demo;
 
+import com.example.demo.repository.JdbcMemberRepository;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 import com.example.demo.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.DataSizeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 
 // 자바 코드로 직접 스프링 빈 등록하는 이유
 // 현재 MemberRepository 는 변경 할 예정이므로 손쉽게 교체하기 위함.
 @Configuration
 public class SpringConfig {
+
+    //@Autowired DataSource dataSource; // 방법 1
+
+    // 방법2
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource=dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -18,6 +35,6 @@ public class SpringConfig {
 
     @Bean
     public  MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
--- 스프링입문/src/main/java/com/example/demo/controller/MemberController.java
+++ 스프링입문/src/main/java/com/example/demo/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -21,7 +24,7 @@ public class MemberController {
         return "members/createMemberForm";
     }
     @PostMapping("/members/new")
-    public String create(MemberForm form){
+    public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
 
@@ -29,5 +32,12 @@ public class MemberController {
 
         //home 으로~
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers(); // 모든 회원을  members에 담아둠.
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
