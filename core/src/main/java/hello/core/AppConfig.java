304ae18ee22308490a43b8dc1f5885df47e7446a
kakjzi <anmern3@naver.com>
2021-11-26 17:10:03 +0900

    Update: Singleton Container!

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -13,19 +13,27 @@ import org.springframework.context.annotation.Configuration;
 //DI 컨테이너
 @Configuration
 public class AppConfig {
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+    // 1. 호출 2번인데 싱글톤이 실제로 깨질까 안깨질까?  -> ConfigurationSingletonTest 참고
+    // 4. 한번만 호출됨
+    // 5. 이유는 AppConfig@CGLIB 코드에 빈이 존재하면 이미 존재하는 빈을 반환하고 없으면 새로 생성하고 컨테이너에 등록
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- core/src/test/java/hello/core/singleton/SingletonService.java
+++ core/src/test/java/hello/core/singleton/SingletonService.java
@@ -1,12 +1,5 @@
 package hello.core.singleton;
-// ## 싱글톤 문제점
-//1. 싱글톤 패턴을 구현하는 코드 자체가 많이 들어감
-//2. 클라이언트가 구체 클래스에 의존함. ->DIP,OCP 위반
-//3. 테스트가 어렵다.
-//4. private 생성자로 자식 클래스를 만들기 어렵다.
-//5. 결론적으로 유연성이 떨어짐.
 
-// -> 스프링컨테이너는 위의 문제를 해결하면서 싱글톤으로 객체를 만들어줌.  ㄷㄷ
 public class SingletonService {
 
     private static final SingletonService instance = new SingletonService();
