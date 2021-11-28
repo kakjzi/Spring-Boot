23e34962e0a2200cae4199194e34ac4361a236f3
kakjzi <anmern3@naver.com>
2021-11-28 21:55:33 +0900

    Update: AutoWiredTest (if No injection target)

---
+++ core/src/test/java/hello/core/autowired/AutoWiredTest.java
@@ -0,0 +1,44 @@
+package hello.core.autowired;
+
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
+
+import java.util.Optional;
+
+public class AutoWiredTest {
+
+    @Test
+    @DisplayName("")
+    void AutoWiredOption() {
+        //given
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+        //when
+
+        //then
+
+    }
+
+    static class TestBean {
+        // 자동주입대상이 없으면 메소드자체 호출안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+        // 자동주입대상이 없으면 null
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+        // 자동주입대상이 없으면 Option.empty
+        @Autowired
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
+
+}
