caa0514745e296bac047c7e6d98722c5ed3d2a0e
kakjzi <anmern3@naver.com>
2021-12-09 17:04:11 +0900

    FIX: HandlerMapping & HandlerAdapter Description

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * HandlerAdapter
+ * 0 = RequestMappingHandlerAdapter : @RequestMapping
+ * 1 = HttpRequestHandlerAdapter : HttpRequestHandler 처리
+ * 2 = SimpleControllerHandlerAdapter : Controller 인터페이스 처리
+ *
+ * HandlerMapping
+ * 0 = RequestMappingHandlerMapping : @RequestMapping
+ * 1 = BeanNameUrlHandlerMapping : 스프링 빈의 이름으로 핸드러를 찾는다.
+ */
+
+
+@Component("/springmvc/old-controller")// BeanNameUrlHandlerMapping
+
+public class OldController implements Controller {  //SimpleControllerHandlerAdapter
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
