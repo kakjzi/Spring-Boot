99ecff70bf0b9e4966fdf4b8f321fcf32a26a318
kakjzi <anmern3@naver.com>
2021-12-16 11:30:27 +0900

    Update: redirectAttributes 사용

--- item-service/src/main/java/hello/itemservice/web/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -71,7 +72,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         //파라미터의 클래스의 이름을 소문자로 바꿔 name 으로 넣어줌.
         // Item -> item
@@ -90,6 +91,16 @@ public class BasicItemController {
         itemRepository.save(item);
         return "basic/item";
     }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        // redirectAttributes 는 URL 인코딩, pathVariable, 쿼리 파라미터 처리 해줌
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+    }
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model){
         Item item = itemRepository.findById(itemId);
