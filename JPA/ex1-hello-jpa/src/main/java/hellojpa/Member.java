9e59b4f42a12ec886898c976c0377d3414b97ce3
kakjzi <anmern3@naver.com>
2021-12-30 21:36:02 +0900

    ADD: 필드,컬럼매핑

--- JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ JPA/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,28 +1,35 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
+    @Column(name = "name") //DB- COL NAME
+    private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private Integer age;
 
-    public String getName() {
-        return name;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    /*@Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;*/
+    // ==
+    private LocalDateTime createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob  //DB에 큰 컨텐츠 삽입 시
+    private String description;
+
+    @Transient //매핑제외
+    private int temp;
+
 }
